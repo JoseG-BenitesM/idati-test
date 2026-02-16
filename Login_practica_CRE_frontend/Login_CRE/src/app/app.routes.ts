f99a7ac839be7db65491a8eca5126ed7ccec5e74
Edgar-QuispeT <equispetapia8@gmail.com>
2026-02-15 22:37:05 -0500

    rutas y cambios minimos

--- Login_practica_CRE_frontend/Login_CRE/src/app/app.routes.ts
+++ Login_practica_CRE_frontend/Login_CRE/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './autenticacion/login/login';
+import { Usuarios } from './paginas/usuarios/usuarios';
+import { InicioUsuario } from './paginas/inicio-usuario/inicio-usuario';
+import { RestContra } from './autenticacion/rest-contra/rest-contra';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: Login},
+    {path: 'usuarios', component: Usuarios},
+    {path: 'inicio-usuario', component: InicioUsuario},
+    {path: 'restaurar-contraseña', component: RestContra}
+];
