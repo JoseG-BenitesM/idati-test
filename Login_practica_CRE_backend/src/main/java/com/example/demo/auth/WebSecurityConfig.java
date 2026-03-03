44e70b6151845c2e565be1ea3433df44667c2341
JGBM <josegbenitesm@gmail.com>
2026-03-02 20:53:52 -0500

    a

--- Login_practica_CRE_backend/src/main/java/com/example/demo/auth/WebSecurityConfig.java
+++ Login_practica_CRE_backend/src/main/java/com/example/demo/auth/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://login-idate.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
