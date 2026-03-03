92dded27e13b2215b8248841512d29f266749e2a
Jeison0729 <farfan2@hotmail.es>
2026-03-02 21:45:12 -0500

    Webconfig corregido

--- Login_practica_CRE_backend/src/main/java/com/example/demo/auth/WebSecurityConfig.java
+++ Login_practica_CRE_backend/src/main/java/com/example/demo/auth/WebSecurityConfig.java
@@ -77,7 +77,9 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://login-idate.netlify.app"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://login-idate.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
