aa0981ba53997884812a56e36f6104e48a1da290
Jeison0729 <farfan2@hotmail.es>
2026-03-06 23:35:37 -0500

    Update CorreoService.java

--- Login_practica_CRE_backend/src/main/java/com/example/demo/solicitud/CorreoService.java
+++ Login_practica_CRE_backend/src/main/java/com/example/demo/solicitud/CorreoService.java
@@ -10,7 +10,7 @@ import java.net.http.HttpResponse;
 @Service
 public class CorreoService {
     
-    private final String API_KEY = "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";
+    private final String API_KEY = "re_ZDDqSSUH_q7Djt3FEUMoUKWpbRyj8E3rP";
     
     public void enviarCorreo(String destinatario, String asunto, String cuerpo) {
         System.out.println("=== INTENTANDO ENVIAR CORREO A: " + destinatario);
@@ -19,10 +19,10 @@ public class CorreoService {
             
             String json = String.format(
                     "{" +
-                            "\"sender\": {\"email\": \"idaticre@gmail.com\"}," +
-                            "\"to\": [{\"email\": \"%s\"}]," +
+                            "\"from\": \"onboarding@resend.dev\"," +
+                            "\"to\": [\"%s\"]," +
                             "\"subject\": \"%s\"," +
-                            "\"textContent\": \"%s\"" +
+                            "\"text\": \"%s\"" +
                             "}",
                     destinatario, asunto, cuerpo
             );
@@ -30,8 +30,8 @@ public class CorreoService {
             System.out.println("=== JSON: " + json);
             
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.brevo.com/v3/smtp/email"))
-                    .header("api-key", API_KEY)
+                    .uri(URI.create("https://api.resend.com/emails"))
+                    .header("Authorization", "Bearer " + API_KEY)
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(json))
                     .build();
@@ -39,8 +39,8 @@ public class CorreoService {
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
             
-            System.out.println("=== BREVO STATUS: " + response.statusCode());
-            System.out.println("=== BREVO BODY: " + response.body());
+            System.out.println("=== RESEND STATUS: " + response.statusCode());
+            System.out.println("=== RESEND BODY: " + response.body());
             
         } catch (Exception e) {
             System.err.println("=== ERROR ENVIANDO CORREO: " + e.getMessage());
